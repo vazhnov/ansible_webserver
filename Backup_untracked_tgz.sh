8e2e771e56bb05f543a91d02105626358a0a1fd3
Aleksey Vazhnov <vazhnov@yandex.ru>
2016-05-21 16:17:01 +0500

    Added Backup_untracked_tgz.sh

---
+++ Backup_untracked_tgz.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script backups untracked git files to tar archive.
+# Creates directory ~/tmp/ansible_webserver_backup
+# If you need another directory, create symlink or pass
+# your path as an argument.
+
+# See 'man bash', chapter 'Parameter Expansion' to understand
+# how to set default values.
+# No need in quotes here!
+BACKUPDIR=${1:-~/tmp/ansible_webserver_backup}
+echo "${BACKUPDIR}"
+test -d "${BACKUPDIR}" || mkdir -p "${BACKUPDIR}"
+git ls-files --ignored --exclude-standard -z | xargs -0 tar -czvf "${BACKUPDIR}/backup-untracked_$(date +%F_%H-%M-%S).tgz"
