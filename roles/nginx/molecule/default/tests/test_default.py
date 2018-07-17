81c81fd3be38386f118e9c6712eb5f944ac0456b
Alexey Vazhnov <vazhnov@yandex.ru>
2018-07-17 13:53:18 +0300

    Nginx test_default.py: removed /etc/hosts testing

--- roles/nginx/molecule/default/tests/test_default.py
+++ roles/nginx/molecule/default/tests/test_default.py
@@ -1,18 +1,3 @@
-import os
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_nginx_is_installed(host):
     nginx = host.package("nginx-full")
     assert nginx.is_installed
