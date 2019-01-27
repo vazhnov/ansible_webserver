8f80d0c82e60dc74934161e44930c8f6c5c91819
Alexey Vazhnov <vazhnov@yandex.ru>
2019-01-27 19:10:58 +0300

    Backup_untracked_*.sh: minor changes

--- Backup_untracked_git.sh
+++ Backup_untracked_git.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 # This script backups untracked git files to another git repository.
 # Creates directory ../ansible_webserver_private
 # If you need another directory, create symlink or pass
 # your path as an argument.
 
 # See 'man bash', chapter 'Parameter Expansion' to understand
-# how to set default values.
-# No need in quotes here!
+# how to set default values. No need in quotes here!
 BACKUPDIR=${1:-../ansible_webserver_private}
-echo "${BACKUPDIR}"
+echo "BACKUPDIR: ${BACKUPDIR}"
+
 test -d "${BACKUPDIR}" || mkdir -vp "${BACKUPDIR}"
 git ls-files --others -z | xargs -0 cp --parents -p -t "${BACKUPDIR}"
 cd "${BACKUPDIR}"
--- Backup_untracked_tgz.sh
+++ Backup_untracked_tgz.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 # This script backups untracked git files to tar archive.
-# Creates directory ~/tmp/ansible_webserver_backup
+# Creates directory ../ansible_webserver_backup
 # If you need another directory, create symlink or pass
 # your path as an argument.
 
 # See 'man bash', chapter 'Parameter Expansion' to understand
-# how to set default values.
-# No need in quotes here!
-BACKUPDIR=${1:-~/tmp/ansible_webserver_backup}
+# how to set default values. No need in quotes here!
+BACKUPDIR=${1:-../ansible_webserver_backup}
+
 test -d "${BACKUPDIR}" || mkdir -vp "${BACKUPDIR}"
 git ls-files --others -z | xargs -0 tar -czvf "${BACKUPDIR}/backup-untracked_$(date +%F_%H-%M-%S).tgz"
